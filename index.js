c259d0300d1596b756b61d51729d72e34edb9035
George Sofianos <georgesofianosgr@gmail.com>
2025-01-30 11:25:10 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+require('dotenv').config()
+
+const fs = require('fs');
+const path = require('path');
+const util = require('util')
+
+const { callAI } = require('./openai')
+
+// Function to load scenario dynamically based on the argument
+function loadScenario(scenarioName) {
+  try {
+    const scenarioPath = path.resolve(__dirname, 'scenarios', `${scenarioName}.js`);
+
+    // Check if the scenario file exists
+    if (!fs.existsSync(scenarioPath)) {
+      console.log(`Scenario file ${scenarioName}.js not found.`);
+      return null;
+    }
+
+    // Dynamically import the scenario file
+    return require(scenarioPath);
+  } catch (err) {
+    console.error('Error loading scenario:', err);
+    return null;
+  }
+}
+
+async function runScenario(name) {
+  const scenario = await loadScenario(name);
+
+  const promises = scenario.workflows.map(async workflow => {
+    const start = performance.now();
+
+    const res = await callAI({
+      systemPrompt: scenario.systemPrompt,
+      functionDefinition: scenario.functionDefinition,
+      messages: workflow.messages,
+      data: workflow.data,
+    });
+
+    const end = performance.now();
+    const elapsedSeconds = (end - start) / 1000; // Convert milliseconds to seconds
+
+
+
+    console.log("\nOutput: \n", util.inspect(res, { showHidden: false, depth: null, colors: true }))
+    console.log(`Time elapsed: ${elapsedSeconds.toFixed(2)} seconds`);
+
+  });
+
+
+  await Promise.all(promises);
+}
+
+
+async function main() {
+  const args = process.argv.slice(2);
+  const name = args[0];
+
+  if (!name) {
+    console.log('Please provide a scenario name.');
+  } else {
+    await runScenario(name);
+  }
+}
+
+main();
---
+++ openai.js
@@ -0,0 +1,73 @@
+const util = require('util')
+const OpenAI = require('openai');
+
+const openaiClient = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+
+const callAI = async ({
+  systemPrompt,
+  functionDefinition,
+  messages,
+  data,
+}) => {
+
+  const dataMessages = createDataMessages(data);
+  const messagePayload = [
+    {
+      role: 'system',
+      content: systemPrompt,
+    },
+    ...messages,
+    ...dataMessages
+  ]
+
+  if (process.env.SHOW_INPUT === 'true') {
+    console.log("Input:\n", util.inspect(messagePayload, { showHidden: false, depth: null, colors: true }))
+  }
+
+  const response = await openaiClient.chat.completions.create({
+    messages: messagePayload,
+    model: process.env.MODEL,
+    temperature: 1,
+    tools: [{ type: 'function', function: functionDefinition }],
+    tool_choice: 'required',
+  });
+
+  const called = response.choices[0].message.tool_calls?.find(
+    toolCall => toolCall.function.name === functionDefinition.name,
+  );
+  if (!called) {
+    throw new Error("function wasn't called");
+  }
+
+  const result = JSON.parse(called.function.arguments);
+
+  if (!result) {
+    console.error('Failed with response: ', {
+      response: response.choices[0].message.content,
+      progress: result,
+      retry,
+    });
+  }
+
+  return result;
+
+}
+
+const createDataMessages = (data) => {
+  return data?.flatMap(d => ([
+    {
+      role: 'assistant',
+      content: d.prompt,
+    }, {
+      role: 'assistant',
+      content: d.content,
+    }
+  ])) ?? [];
+}
+
+module.exports = {
+  callAI
+}
